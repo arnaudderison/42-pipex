417939927d3f7091c33394fb1356ac6bd0bc5cd9
arnaudderison <arnaud.derison@hotmail.com>
2024-01-05 23:25:31 +0100

    this day

--- include/pipex.h
+++ include/pipex.h
@@ -6,13 +6,35 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 21:44:53 by arnaud            #+#    #+#             */
-/*   Updated: 2024/01/05 21:45:41 by arnaud           ###   ########.fr       */
+/*   Updated: 2024/01/05 22:42:43 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
-#define PIPEX_H
-#include "libft.h"
-#include "printf.h"
+# define PIPEX_H
+
+# include "ft_printf.h"
+# include "libft.h"
+
+# define RED "\x1B[31m"
+# define GREEN "\x1B[32m"
+# define YELLOW "\x1B[33m"
+# define BLUE "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYN "\x1B[36m"
+# define WHITW "\x1B[37m"
+# define RESET "\x1B[0m"
+
+# define NB_ARGS_ERR "Number(s) of args is invalide\n"
+# define NO_ENV_ERR "Undetected environment variable\n"
+
+typedef struct s_pipex
+{
+	char	**cmds;
+	char	***s_cmds;
+	int		cmdc;
+	int		fd_input;
+	int		fd_output;
+} t_pipex
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,17 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 20:05:22 by arnaud            #+#    #+#             */
-/*   Updated: 2024/01/02 20:33:07 by arnaud           ###   ########.fr       */
+/*   Updated: 2024/01/05 22:17:10 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../include/pipex.h"
 
-int main(void)
+int	main(int argc, char **argv, char **env)
 {
-    ft_printf("coucou");
-    return 1;
+	if (argc != 5 || !argv)
+		return (ft_putstr_fd(RED NB_ARGS_ERR RESET, 2));
+	if (!env)
+		return (ft_putstr_fd(RED NO_ENV_ERR RESET, 2));
+	return (1);
 }
