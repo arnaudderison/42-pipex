42f0d83db1f51e8dc76ea84d62b75656dd2c012e
arnaudderison <arnaud.derison@hotmail.com>
2024-01-07 21:04:29 +0100

    add s_cmds in pipex type

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 21:44:53 by arnaud            #+#    #+#             */
-/*   Updated: 2024/01/07 16:35:56 by arnaud           ###   ########.fr       */
+/*   Updated: 2024/01/07 20:28:51 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 # include "ft_printf.h"
 # include "libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/wait.h>
 
 # define RED "\x1B[31m"
 # define GREEN "\x1B[32m"
@@ -22,9 +25,9 @@
 # define BLUE "\x1B[34m"
 # define MAG "\x1B[35m"
 # define CYN "\x1B[36m"
-# define WHITW "\x1B[37m"
 # define RESET "\x1B[0m"
 
+# define NULL_PTR "NULL Pointer ERROR...\n"
 # define NB_ARGS_ERR "Number(s) of args is invalide\n"
 # define NO_ENV_ERR "Undetected environment variable\n"
 
@@ -36,5 +39,9 @@ typedef struct s_pipex
 	int		fd_input;
 	int		fd_output;
 }			t_pipex;
+
+void		init(t_pipex *pipex, char **argv, char **env, int argc);
+void		exit_pipex(t_pipex *pipex, char *msg);
+char		**cleantab(char **tab, int size);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 20:05:22 by arnaud            #+#    #+#             */
-/*   Updated: 2024/01/05 22:17:10 by arnaud           ###   ########.fr       */
+/*   Updated: 2024/01/07 20:35:49 by arnaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	if (argc != 5 || !argv)
+	t_pipex	pipex;
+
+	if (argc < 5 || !argv)
 		return (ft_putstr_fd(RED NB_ARGS_ERR RESET, 2));
 	if (!env)
 		return (ft_putstr_fd(RED NO_ENV_ERR RESET, 2));
+	init(&pipex, argv, env, argc);
 	return (1);
 }
